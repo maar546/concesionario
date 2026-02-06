bb61ed4f13cbc24d221a52c184bb3d07a4c08174
lmbnkk <limbonikk@gmail.com>
2026-02-06 17:21:37 +0100

    Eliminado AppController y modificado CarController + MainController

--- src/main/java/com/svalero/practicasfeb/controller/CarController.java
+++ src/main/java/com/svalero/practicasfeb/controller/CarController.java
@@ -1,4 +1,62 @@
 package com.svalero.practicasfeb.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
 public class CarController {
+
+    @FXML private TextField tfMatricula, tfMarca, tfModelo, tfPrecioBase, tfUltimaRevision;
+    @FXML private Label lbAvisoMatricula, lbAvisoMarca;
+    @FXML private Button btnRegistrarCoche, btnModificarCoche, btnEliminarCoche;
+
+    @FXML
+    public void registrarCoche(ActionEvent event) {
+        // TODO CAMBIAR Y ADAPTAR PARA NUESTRO TRABAJO
+        String matricula = tfTitulo.getText();
+        String marca = tfPrecio.getText();
+        String modelo = tfGenero.getText();
+        float precioBase = dpFecha.getValue();
+        LocalDate digital = cbDigital.isSelected();
+
+        // Validación de campos obligatorios
+        if (titulo.isEmpty() || precioStr.isEmpty()) {
+            lbStatusV.setText("Error: Título y Precio son obligatorios");
+            return;
+        }
+
+        try {
+            float precio = Float.parseFloat(precioStr);
+            Videojuego nuevoV = new Videojuego(titulo, precio, genero, fecha, digital);
+
+            // Al estar envuelta, se actualiza la tabla (UI) y el repositorio a la vez.
+            listaVideojuegos.add(nuevoV);
+
+            lbStatusV.setText("Videojuego registrado correctamente");
+            limpiarCamposVideojuego();
+        } catch (NumberFormatException e) {
+            lbStatusV.setText("Error: El precio debe ser un número");
+        }
+    }
+
+    @FXML
+    public void modificarCoche(ActionEvent event) {
+        // TODO HACER EL METODO
+    }
+
+    @FXML
+    public void eliminarCoche(ActionEvent event) {
+        // TODO HACER EL METODO
+    }
+
+    @FXML
+    public void initialize() {
+        // Limpiamos todo
+        lbAvisoMatricula.setText("");
+        lbAvisoMarca.setText("");
+    }
 }
--- src/main/java/com/svalero/practicasfeb/controller/MainController.java
+++ src/main/java/com/svalero/practicasfeb/controller/MainController.java
@@ -2,16 +2,32 @@ package com.svalero.practicasfeb.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import java.io.IOException;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane areaContenido;
+
+    @FXML
+    public void verCarView(ActionEvent event) {
+        try {
+
+            // Cargamos el archivo FXML.
+            Parent view = FXMLLoader.load(getClass().getResource("/com/svalero/practicasfeb/car_view.fxml"));
+
+            // 2. Limpiamos lo que haya en el AnchorPane y añadimos la nueva vista
+            areaContenido.getChildren().setAll(view);
+
+        } catch (IOException e) {
+            System.err.println("Error al cargar Car_View: " + e.getMessage());
+        }
     }
 }
