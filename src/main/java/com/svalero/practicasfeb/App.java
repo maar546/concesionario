cfe16c89b375d6d15e5f69220b3e6fc44f568b68
lmbnkk <limbonikk@gmail.com>
2026-02-06 16:16:30 +0100

    Modificado App y Car_view + main_view

--- src/main/java/com/svalero/practicasfeb/App.java
+++ src/main/java/com/svalero/practicasfeb/App.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Car Dealer - SValero");
         stage.setScene(scene);
         stage.show();
     }
 }
+
