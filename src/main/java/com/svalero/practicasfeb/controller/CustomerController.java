2b874f855d7ecda1da52c4045472e1e39a03bcb1
itzia <a29539@svalero.com>
2026-02-07 13:58:30 +0100

    Add controller: Se ha añadido los controllers

--- src/main/java/com/svalero/practicasfeb/controller/CustomerController.java
+++ src/main/java/com/svalero/practicasfeb/controller/CustomerController.java
@@ -1,36 +1,194 @@
 package com.svalero.practicasfeb.controller;
 
+import com.svalero.practicasfeb.model.Customer;
+import com.svalero.practicasfeb.repository.CustomerDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.time.LocalDate;
+import java.util.List;
 
 public class CustomerController {
+
+    @FXML private TextField tfNombre, tfDni, tfEdad;
+    @FXML private CheckBox cbEsPremium;
+    @FXML private DatePicker dpFechaAlta;
+    @FXML private Label lbAvisoNombre, lbAvisoDni, lbAvisoCustomer, lbContadorCustomer;
+    @FXML private Button btnRegistrarCustomer, btnModificarCustomer, btnEliminarCustomer, btnAnteriorCustomer, btnSiguienteCustomer;
+
     @FXML
     public void registrarCustomer(ActionEvent event) {
+
+        String nombre = tfNombre.getText();
+        String dni = tfDni.getText();
+        String edadStr = tfEdad.getText();
+        Boolean esPremium = cbEsPremium.isSelected();
+        LocalDate fechaAlta = dpFechaAlta.getValue();
+
+        // Validación de campos obligatorios
+        boolean error = false;
+        if (nombre.isEmpty()) {lbAvisoNombre.setText("Error: Name is mandatory");error = true;}
+        if (dni.isEmpty()) {lbAvisoDni.setText("Error: DNI is mandatory");error = true;
+
+        if (error) return;
+
+        try {
+
+                Customer newCustomer = new Customer(nombre, dni, edadStr, esPremium, fechaAlta);
+                customerDAO.save(newCustomer);
+                lbAvisoCustomer.setText("Customer registration completed");
+                btnRegistrarCustomer.setDisable(true);
+                cargarDatosCustomer();
+            } catch (Exception e) {
+            // ESTO ES LO QUE TE FALTA PARA QUE COMPILE
+            // Aquí capturamos el "throws Exception" que declaraste en CarDAO
+            e.printStackTrace();
+            lbAvisoCustomer.setText("Error: Database operation failed");
+        }
+        }
     }
 
     @FXML
     public void modificarCustomer(ActionEvent event) {
 
-        // TODO Obtener datos a modificar
+        String nombre = tfNombre.getText();
+        String dni = tfDni.getText();
+        String edadStr = tfEdad.getText();
+        Boolean esPremium = cbEsPremium.isSelected();
+        LocalDate fechaAlta = dpFechaAlta.getValue();
 
+        // Validación de campos obligatorios
+        boolean error = false;
+        if (nombre.isEmpty()) { lbAvisoNombre.setText("Error: No name selected to modify"); error = true; }
+        if (dni.isEmpty()) { lbAvisoDni.setText("Error: DNI is mandatory"); error = true; }
 
-        // TODO Validar que ha seleccionado algo
+        if (error) return;
 
+        try {
+            if (nombre = null ) {
+                lbAvisoNombre.setText("Error: Name can not be empty");
+                return;
+            }
+
+            Customer customerModificado = new Customer(nombre, dni, edadStr, esPremium, fechaAlta);
+            customerDAO.update(customerModificado);
+
+            lbAvisoCustomer.setText("Customer updated");
+
+            int indiceGuardado = indiceActual;
+            cargarDatosCustomer();
+            indiceActual = indiceGuardado;
+            mostrarCustomer(indiceActual);
+        } catch (Exception e) {
+            e.printStackTrace();
+            lbAvisoCustomer.setText("Error updating database");
+        }
     }
+
 
     @FXML
     public void eliminarCustomer(ActionEvent event) {
-        // TODO
+
+        String nombre = tfNombre.getText();
+
+        try {
+            customerDAO.delete(nombre);
+            lbAvisoCustomer.setText("Customer deleted");
+
+            cargarDatosCustomer();
+
+            if (listaCustomer.isEmpty()) {
+                limpiarCamposCustomer();
+                lbContadorCustomer.setText("0/0");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            lbAvisoCustomer.setText("Error: Couldn't delete");
+        }
     }
 
     // Borrar todos los campos
     @FXML
     public void limpiarCamposCustomer() {
-        // TODO
+        tfNombre.clear();
+        tfNombre.setEditable(true);
+
+        tfDni.clear();
+        tfEdad.clear();
+        cbEsPremium.clear();
+        dpFechaAlta.setValue(null);
+
+        btnRegistrarCustomer.setDisable(false);
+        btnModificarCustomer.setDisable(true);
+        btnEliminarCustomer.setDisable(true);
     }
+
+
+    // MAPEO GENERAL
+    private List<Customer> listaCustomer;
+    private int indiceActual = 0;
+    private CustomerDAO customerDAO = new CustomerDAO();
+
 
     @FXML
     public void initialize() {
-        // TODO
+        // Limpieza de avisos
+        lbAvisoNombre.setText("");
+        lbAvisoDni.setText("");
+        cargarDatosCustomer();
+    }
+
+    private void mostrarCustomer(int indice) {
+        Customer customer = listaCustomer.get(indice);
+
+        tfNombre.setText(customer.getNombre());
+        tfNombre.setEditable(false); // para no poder modificar la matrícula
+
+        tfDni.setText(customer.getDni());
+        tfEdad.setText(customer.getEdad());
+        cbEsPremium.setSelected(customer.getEsPremium());
+        dpFechaAlta.setValue(customer.getFechaAlta());
+    }
+
+    // MOSTRAR DATOS EN LA PANTALLA AL ENTRAR EN ENTITY/CUSTOMER
+    private void cargarDatosCustomer() {
+        btnModificarCustomer.setDisable(false);
+        btnEliminarCustomer.setDisable(false);
+        btnRegistrarCustomer.setDisable(true);
+
+        try {
+            // Llamada a findAll() del DAO
+            listaCustomer = customerDAO.findAll();
+
+            if (listaCustomer != null && !listaCustomer.isEmpty()) {
+                indiceActual = 0;
+                mostrarCustomer(indiceActual);
+            } else {
+                lbAvisoCustomer.setText("Database is empty");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            lbAvisoCustomer.setText("Error loading data");
+        }
+    }
+
+    @FXML
+    private void siguienteCustomer(ActionEvent event) {
+        if (listaCustomer != null && indiceActual < listaCustomer.size() - 1) {
+            indiceActual++;
+            mostrarCustomer(indiceActual);
+        }
+    }
+
+    @FXML
+    private void anteriorCustomer(ActionEvent event) {
+        if (listaCustomer != null && indiceActual > 0) {
+            indiceActual--;
+            mostrarCustomer(indiceActual);
+        }
     }
 }
